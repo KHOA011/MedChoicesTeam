3827b1affc7ac7e9c2fa122007c9ff21916028cc
Khoa Le <kle21@huskers.unl.edu>
2023-10-18 20:36:53 -0500

    update user's record and add doctor info

--- medchoices/src/Summary.js
+++ medchoices/src/Summary.js
@@ -1,81 +1,223 @@
-import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Summarypage = () => {
-  const location = useLocation();
-  const { userResponse, questions } = location.state || {};
-  const navigate = useNavigate();
+    const location = useLocation();
+    const { userResponse, questions, number } = location.state || {};
+    const navigate = useNavigate();
 
-  if (!userResponse || !questions) {
-    return (
-      <div>
-        <h1>Error: Missing Data</h1>
-        <p>Sorry, there was an issue loading the summary data.</p>
-      </div>
-    );
-  }
+    const [selectedDoctor, setSelectedDoctor] = useState("");
+    const [doctorInfo, setDoctorInfo] = useState("");
+    const [doctorList, setDoctorList] = useState([]);
+    const [downloaded, setDownloaded] = useState(false);
+
+    useEffect(() => {
+        // Function to load doctor list based on the survey type
+        const loadDoctorList = () => {
+            if (
+                number === "depression" ||
+                number === "bipolar" ||
+                number === "anxiety"
+            ) {
+                // Load the mental health doctor list
+                fetch("/doctorlist/mental_health_doctors.txt")
+                    .then((response) => response.text())
+                    .then((text) => {
+                        const doctors = text
+                            .split("\n")
+                            .map((line) => line.trim());
+                        setDoctorList(doctors);
+                    });
+            } else {
+                // Load the physical health doctor list
+                fetch("/doctorlist/physical_health_doctors.txt")
+                    .then((response) => response.text())
+                    .then((text) => {
+                        const doctors = text
+                            .split("\n")
+                            .map((line) => line.trim());
+                        setDoctorList(doctors);
+                    });
+            }
+        };
+
+        // Fetch the doctor list
+        loadDoctorList();
+    }, [number]);
+
+    const handleDoctorSelection = (selectedDoctorName) => {
+        // Fetch the content of the selected doctor's file
+        fetch(`/doctorlist/${selectedDoctorName}.txt`)
+            .then((response) => response.text())
+            .then((text) => {
+                // Set the doctor information in the state
+                setDoctorInfo(text);
+            })
+            .catch((error) => {
+                // Handle errors, e.g., the selected doctor file not found
+                console.error("Error fetching doctor information:", error);
+            });
+    };
+
+    const handleDownloadSummary = () => {
+        // Create the text content for the summary
+        const summaryText = `Summary for Survey ${number}\n\nUser Information:\nAge: ${
+            userResponse.age
+        }\nEmail: ${userResponse.email}\nPhone Number: ${
+            userResponse.phone
+        }\n\nEvaluation:\nAverage Grade: ${averageGrade.toFixed(
+            2
+        )} (${healthConditionMessage})\n\nQuestions and Answers:\n${questions
+            .map(
+                (question, index) =>
+                    `Q${index + 1}: ${question}\nAnswer: ${
+                        userResponse.surveyResponses[index]
+                    }`
+            )
+            .join(
+                "\n\n"
+            )}\n\nSelected Doctor:\n${selectedDoctor}\n${doctorInfo}`;
+
+        // Create a Blob with the summary text
+        const blob = new Blob([summaryText], { type: "text/plain" });
+
+        // Create a URL for the Blob
+        const url = window.URL.createObjectURL(blob);
+
+        // Create a link element to trigger the download
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "survey_summary.txt";
+
+        // Trigger a click event to initiate the download
+        a.click();
+
+        // Set the downloaded state to true and show the confirmation message
+        setDownloaded(true);
+    };
+
+    if (!userResponse || !questions || !number) {
+        return (
+            <div>
+                <h1>Error: Missing Data</h1>
+                <p>Sorry, there was an issue loading the summary data.</p>
+            </div>
+        );
+    }
+
     // Calculate the average grade for numerical responses
     const numericalResponses = userResponse.surveyResponses.filter(
-      (response) => typeof response === 'number'
+        (response) => typeof response === "number"
     );
-  
+
     const sum = numericalResponses.reduce((acc, value) => acc + value, 0);
     const averageGrade = sum / numericalResponses.length;
 
     const getHealthConditionMessage = (averageGrade) => {
-      if (averageGrade < 3) {
-        return 'Healthy Condition';
-      } else if (averageGrade >= 3 && averageGrade < 5) {
-        return 'Mild Health Problem';
-      } else if (averageGrade >= 5 && averageGrade < 7) {
-        return 'Moderate Health Problem';
-      } else {
-        return 'Severe Health Problem';
-      }
+        if (averageGrade < 3) {
+            return "Healthy Condition";
+        } else if (averageGrade >= 3 && averageGrade < 5) {
+            return "Mild Health Problem";
+        } else if (averageGrade >= 5 && averageGrade < 7) {
+            return "Moderate Health Problem";
+        } else {
+            return "Severe Health Problem";
+        }
     };
     const healthConditionMessage = getHealthConditionMessage(averageGrade);
 
     const questionAnswerStyle = {
-      fontSize: '17px', // Set the font size to 14px or your preferred size
-      paddingLeft: '20px', // Adjust the tab spacing
+        fontSize: "17px",
+        paddingLeft: "20px",
     };
     const sectionStyle = {
-      color: 'white', // White text color
-      padding: '20px', // Add some padding for spacing
+        color: "white",
+        padding: "20px",
     };
 
-    
-  return (
-    <div className="survey-container">
-<section style={sectionStyle} >
-      <h1 >Summary Page</h1>
-      <details>
-        <summary>User Information</summary>
-        <p>Age: {userResponse.age}</p>
-        <p>Email: {userResponse.email}</p>
-        <p>Phone Number: {userResponse.phone}</p>
-      </details>
+    return (
+        <div className="survey-container">
+            <section style={sectionStyle}>
+                <h1>Summary Page for survey {number}</h1>
+                <details>
+                    <summary>User Information</summary>
+                    <p>Age: {userResponse.age}</p>
+                    <p>Email: {userResponse.email}</p>
+                    <p>Phone Number: {userResponse.phone}</p>
+                </details>
 
-      <details>
-        <summary>Evaluation</summary>
-        <p>Average Grade: {averageGrade.toFixed(2)} ({healthConditionMessage})</p>        
-      </details>
+                <details>
+                    <summary>Evaluation</summary>
+                    <p>
+                        Average Grade: {averageGrade.toFixed(2)} (
+                        {healthConditionMessage})
+                    </p>
+                </details>
 
-      <details>
-        <summary>Questions and Answers</summary>
-        <div style={questionAnswerStyle}>
-          {questions.map((question, index) => (
-            <details key={index}>
-              <summary style={questionAnswerStyle}>{question}</summary>
-              <p style={questionAnswerStyle}>Answer: {userResponse.surveyResponses[index]}</p>
-            </details>
-          ))}
+                <details>
+                    <summary>Questions and Answers</summary>
+                    <div style={questionAnswerStyle}>
+                        {questions.map((question, index) => (
+                            <details key={index}>
+                                <summary style={questionAnswerStyle}>
+                                    {question}
+                                </summary>
+                                <p style={questionAnswerStyle}>
+                                    Answer:{" "}
+                                    {userResponse.surveyResponses[index]}
+                                </p>
+                            </details>
+                        ))}
+                    </div>
+                </details>
+
+                <details>
+                    <summary>Suggestions</summary>
+                    <select
+                        value={selectedDoctor}
+                        onChange={(e) => {
+                            setSelectedDoctor(e.target.value);
+                            handleDoctorSelection(e.target.value);
+                        }}
+                    >
+                        <option value="">Select a doctor</option>
+                        {doctorList.map((doctorInfo, index) => {
+                            const [name, specialist] = doctorInfo.split(",");
+                            return (
+                                <option key={index} value={name}>
+                                    {name} - {specialist}
+                                </option>
+                            );
+                        })}
+                    </select>
+                </details>
+
+                {selectedDoctor && (
+                    <button
+                        onClick={handleDownloadSummary}
+                        className="download-button"
+                    >
+                        Download Summary
+                    </button>
+                )}
+
+                {downloaded && (
+                    <div>
+                        <p>
+                            The record has been downloaded. Thank you for your
+                            time.
+                        </p>
+                        <button
+                            onClick={() => navigate("/")}
+                            className="submit-button"
+                        >
+                            Confirm
+                        </button>
+                    </div>
+                )}
+            </section>
         </div>
-      </details>
-      <button onClick={() => navigate('/')} className="submit-button">Confirm</button>
-    </section>
-    </div>
-  );
+    );
 };
 
 export default Summarypage;
--- medchoices/src/Survey.js
+++ medchoices/src/Survey.js
@@ -1,171 +1,178 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
 const Survey = () => {
-  const { number } = useParams();
-  const navigate = useNavigate();
+    const { number } = useParams();
+    const navigate = useNavigate();
 
-  const [questions, setQuestions] = useState([]);
-  const [responses, setResponses] = useState(Array(questions.length).fill(0));
-  const [age, setAge] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
+    const [questions, setQuestions] = useState([]);
+    const [responses, setResponses] = useState(Array(questions.length).fill(0));
+    const [age, setAge] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
 
-  useEffect(() => {
-    async function fetchQuestions() {
-      try {
-        const response = await fetch(`/question/${number}.txt`);
-        if (response.ok) {
-          const text = await response.text();
-          const questionList = text.split('\n').filter((line) => line.trim() !== '');
-          setQuestions(questionList);
-          setResponses(Array(questionList.length).fill(0));
-        } else {
-          console.error('Failed to fetch questions.');
+    useEffect(() => {
+        async function fetchQuestions() {
+            try {
+                const response = await fetch(`/question/${number}.txt`);
+                if (response.ok) {
+                    const text = await response.text();
+                    const questionList = text
+                        .split("\n")
+                        .filter((line) => line.trim() !== "");
+                    setQuestions(questionList);
+                    setResponses(Array(questionList.length).fill(0));
+                } else {
+                    console.error("Failed to fetch questions.");
+                }
+            } catch (error) {
+                console.error("Error fetching questions:", error);
+            }
         }
-      } catch (error) {
-        console.error('Error fetching questions:', error);
-      }
-    }
 
-    fetchQuestions();
-  }, [number]);
+        fetchQuestions();
+    }, [number]);
 
-  const handleRatingChange = (index, value) => {
-    const newResponses = [...responses];
-    newResponses[index] = value;
-    setResponses(newResponses);
-  };
-
-  const goBack = () => {
-    navigate(-1); // Navigate back to the previous page
-  };
-
-  const handleSubmit = () => {
-    // Validation for age
-    if (!/^\d{2}$|^[3-7]\d$|^80$/.test(age)) {
-      alert('Please enter a valid age between 16 and 80.');
-      return;
-    }
-  
-    // Validation for email
-    if (!/^.+@.+\..{2,30}$/.test(email)) {
-      alert('Please enter a valid email (less than 30 characters).');
-      return;
-    }
-  
-    // Validation for phone
-    if (!/^\d{10}$/.test(phone)) {
-      alert('Please enter a valid 9-digit phone number.');
-      return;
-    }
-    // Create an object with user responses
-    const userResponse = {
-      age,
-      email,
-      phone,
-      surveyResponses: responses,
+    const handleRatingChange = (index, value) => {
+        const newResponses = [...responses];
+        newResponses[index] = value;
+        setResponses(newResponses);
     };
 
-    // Redirect to the summary page and pass the user response data
-    navigate('/summary', { state: { userResponse, questions } });
-  };
+    const goBack = () => {
+        navigate(-1); // Navigate back to the previous page
+    };
 
-  const surveyFormStyle = {
-    backgroundColor: '#fff',
-    maxWidth: '500px',
-    margin: '50px auto',
-    padding: '30px 20px',
-    boxShadow: '2px 5px 10px rgba(0, 0, 0, 0.5)',
-  };
-  
-  const formControlStyle = {
-    textAlign: 'left',
-    marginBottom: '25px',
-  };
-  
-  const labelStyle = {
-    display: 'block',
-    marginBottom: '10px',
-  };
-  
-  const inputStyle = {
-    border: '1px solid #777',
-    borderRadius: '2px',
-    fontFamily: 'inherit',
-    padding: '10px',
-    display: 'block',
-    width: '95%',
-  };
-  
-  return (
-    <div className="survey-container">
-      <form id="form" style={surveyFormStyle}>
-        <div key="age" style={formControlStyle}>
-          <label style={labelStyle} htmlFor="age">
-            Age
-          </label>
-          <input
-            type="text"
-            value={age}
-            onChange={(e) => setAge(e.target.value)}
-            style={inputStyle}
-            id="age"
-          />
+    const handleSubmit = () => {
+        // Validation for age
+        if (!/^\d{2}$|^[3-7]\d$|^80$/.test(age)) {
+            alert("Please enter a valid age between 16 and 80.");
+            return;
+        }
+
+        // Validation for email
+        if (!/^.+@.+\..{2,30}$/.test(email)) {
+            alert("Please enter a valid email (less than 30 characters).");
+            return;
+        }
+
+        // Validation for phone
+        if (!/^\d{10}$/.test(phone)) {
+            alert("Please enter a valid 9-digit phone number.");
+            return;
+        }
+        // Create an object with user responses
+        const userResponse = {
+            age,
+            email,
+            phone,
+            surveyResponses: responses,
+        };
+
+        // Redirect to the summary page and pass the user response data
+        navigate("/summary", { state: { userResponse, questions, number } });
+    };
+
+    const surveyFormStyle = {
+        backgroundColor: "#fff",
+        maxWidth: "500px",
+        margin: "50px auto",
+        padding: "30px 20px",
+        boxShadow: "2px 5px 10px rgba(0, 0, 0, 0.5)",
+    };
+
+    const formControlStyle = {
+        textAlign: "left",
+        marginBottom: "25px",
+    };
+
+    const labelStyle = {
+        display: "block",
+        marginBottom: "10px",
+    };
+
+    const inputStyle = {
+        border: "1px solid #777",
+        borderRadius: "2px",
+        fontFamily: "inherit",
+        padding: "10px",
+        display: "block",
+        width: "95%",
+    };
+
+    return (
+        <div className="survey-container">
+            <form id="form" style={surveyFormStyle}>
+                <div key="age" style={formControlStyle}>
+                    <label style={labelStyle} htmlFor="age">
+                        Age
+                    </label>
+                    <input
+                        type="text"
+                        value={age}
+                        onChange={(e) => setAge(e.target.value)}
+                        style={inputStyle}
+                        id="age"
+                    />
+                </div>
+
+                <div key="email" style={formControlStyle}>
+                    <label style={labelStyle} htmlFor="email">
+                        Email
+                    </label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        style={inputStyle}
+                        id="email"
+                    />
+                </div>
+
+                <div key="phone" style={formControlStyle}>
+                    <label style={labelStyle} htmlFor="phone">
+                        Phone Number
+                    </label>
+                    <input
+                        type="tel"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                        style={inputStyle}
+                        id="phone"
+                    />
+                </div>
+
+                {questions.map((question, index) => (
+                    <div key={index} style={formControlStyle}>
+                        <label style={labelStyle} htmlFor={`question-${index}`}>
+                            {question}
+                        </label>
+                        <input
+                            type="number"
+                            min="1"
+                            max="10"
+                            value={responses[index]}
+                            onChange={(e) =>
+                                handleRatingChange(
+                                    index,
+                                    parseInt(e.target.value, 10)
+                                )
+                            }
+                            style={inputStyle}
+                            id={`question-${index}`}
+                        />
+                    </div>
+                ))}
+
+                <button onClick={handleSubmit} className="submit-button">
+                    Submit Survey
+                </button>
+                <button onClick={goBack} className="go-back-link">
+                    Back
+                </button>
+            </form>
         </div>
-
-        <div key="email" style={formControlStyle}>
-          <label style={labelStyle} htmlFor="email">
-            Email
-          </label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            style={inputStyle}
-            id="email"
-          />
-        </div>
-
-        <div key="phone" style={formControlStyle}>
-          <label style={labelStyle} htmlFor="phone">
-            Phone Number
-          </label>
-          <input
-            type="tel"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            style={inputStyle}
-            id="phone"
-          />
-        </div>
-
-        {questions.map((question, index) => (
-          <div key={index} style={formControlStyle}>
-            <label style={labelStyle} htmlFor={`question-${index}`}>
-              {question}
-            </label>
-            <input
-              type="number"
-              min="1"
-              max="10"
-              value={responses[index]}
-              onChange={(e) => handleRatingChange(index, parseInt(e.target.value, 10))}
-              style={inputStyle}
-              id={`question-${index}`}
-            />
-          </div>
-        ))}
-
-        <button onClick={handleSubmit} className="submit-button">
-          Submit Survey
-        </button>
-        <button onClick={goBack} className="go-back-link">
-          Back
-        </button>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default Survey;
