689f4c5b572e89e13e9cfaee6cb9e801a3676b7c
Roy Truong <ktruong3@unl.edu>
2023-10-22 20:12:39 -0500

    Add new page (Do you know) into direction

--- medchoices/src/AppRouter.js
+++ medchoices/src/AppRouter.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Survey from './Survey';
-import SummaryPage from './Summary'; // Corrected import
+import SummaryPage from './Summary';
+import DoYouKnow from './DoYouKnow'; // Corrected import
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -10,6 +11,7 @@ const AppRouter = () => (
       <Route path="/" element={<Login />} />
       <Route path="/survey/:number" element={<Survey />} />
       <Route path="/summary" element={<SummaryPage />} /> {/* Corrected import */}
+      <Route path="/doyouknow" element={<DoYouKnow />}/>
     </Routes>
   </BrowserRouter>
 );
